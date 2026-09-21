f279e1ab50aaff13d210733a413c5a68e1177192
CarlosViachica <guevaracarloseduardo66@gmail.com>
2026-09-21 12:02:52 -0600

    Metodo para actualizar

--- routes/gato.routes.js
+++ routes/gato.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { registrarGato, obtenerGatos } from "../controllers/gato.controller.js";
+import { registrarGato, obtenerGatos, actualizarGato } from "../controllers/gato.controller.js";
 
 
 const router = express.Router();
@@ -19,5 +19,7 @@ router.post(
 );
 router.get("/gatos", obtenerGatos);
 
+
+router.put("/gato/:id", upload.single("imagen"), actualizarGato);
 
 export default router;
